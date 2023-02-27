4014b25b81f8025ad7a9aa29b712d3a86da26cae
yonggyo1125 <yonggyo00@kakao.com>
2023-02-27 19:23:17 +0900

    파일 업로드

--- main/java/controllers/files/FileDeleteController.java
+++ main/java/controllers/files/FileDeleteController.java
@@ -1,6 +1,7 @@
 package controllers.files;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import commons.JSONResult;
 import models.file.FileDeleteService;
 import models.file.FileServiceManager;
 
@@ -16,14 +20,25 @@ public class FileDeleteController extends HttpServlet {
 
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		JSONResult<Object> result = new JSONResult<>();
 		try {
 			FileServiceManager manager = FileServiceManager.getInstance();
 			FileDeleteService service = manager.getFileDeleteService();
 			service.delete(req);
 			
+			result.setSuccess(true);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
+			result.setSuccess(false);
+			result.setMessage(e.getMessage());
 		}
+		
+		ObjectMapper om = new ObjectMapper();
+		String json = om.writeValueAsString(result);
+		resp.setContentType("application/json; charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		out.print(json);
 	}
 
 	@Override
---
+++ main/webapp/js/fileManager.js
@@ -0,0 +1,23 @@
+var koreait = koreait || {};
+koreait.fileManager = {
+	/**
+	 * 파일 업로드 처리 
+	 * 
+	 */
+	upload() {},
+	/**
+	 * 목록 조회 
+	 * 
+	 */
+	gets(gid) {},
+	/**
+	 * 파일 목록 삭제
+	 * 
+	 */
+	deletes(gid) {},
+	/**
+	 * 파일 아이디로 삭제
+	 */
+	delete(id) {}
+	
+};
