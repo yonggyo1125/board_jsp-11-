b9c0734a655827d3d751d1c78499511d7ccad868
yonggyo1125 <yonggyo00@kakao.com>
2023-02-28 20:55:27 +0900

    .

---
+++ main/java/controllers/boards/BoardUpdateController.java
@@ -0,0 +1,23 @@
+package controllers.boards;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/update/*")
+public class BoardUpdateController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+}
